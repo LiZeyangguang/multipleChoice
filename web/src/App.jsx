26d327754ebcb718f6060d4668af971ccf419c53
Noah Tonnesen <noah.tonnesen@gmail.com>
2025-10-23 11:22:18 -0400

    remove unused imports and getSessionId from App.jsx; this is now handled by useQuiz

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,21 +1,9 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { api } from './api';
-import QuestionCard from './components/QuestionCard.jsx';
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Home from "./pages/home";      
 import Login from "./pages/login";
 import SignUp from "./pages/signUp";
 import Quiz from './pages/quiz.jsx';
-
-function getSessionId() {
-  const key = 'quiz-session-id';
-  let id = localStorage.getItem(key);
-  if (!id) {
-    id = Math.random().toString(36).slice(2, 14);
-    localStorage.setItem(key, id);
-  }
-  return id;
-}
 
 export default function App() {
   return (
